f8ee799df054a932653505d15e4f8161920a9715
Lenovo-HK <56672035@qq.com>
2020-07-19 11:26:00 +0800

    vedioV1.0

--- src/0.py
+++ src/0.py
@@ -1,10 +1,3 @@
-'''
-save image from camera:/dev/video0
-
-get image for  detect_object
-
-python3 0.py
-'''
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -59,10 +52,40 @@ def save_image_process(Camera):
         key = cv2.waitKey(1)
         if key & 0xFF == ord('q'):
             break 
+
+
+def save_vedio_process(Camera):
+    mkdir(path+"/data")
+    video = v4l2capture.Video_device(Camera)
+
+    video.create_buffers(1)
+    video.queue_all_buffers()
+    video.start() 
+    
+    video.set_format(424,240, fourcc='XVID')
+    #video.set_format(160,120, fourcc='MJPG')
+    out = cv2.VideoWriter('save.avi',vedio,20,(420,240))   #name,frame rate,format
+
+    while(True):
+        select.select((video,), (), ())
+        image_data = video.read_and_queue()
+        frame = cv2.imdecode(np.frombuffer(image_data, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+        out.write(frame)
+        
+        if key & 0xFF == ord('q'):
+            break 
+        
+
+    out.release()
+
+
+
+ 
  
 
 
 if __name__ == '__main__':
-    save_image_process("/dev/video2")
+    save_vedio_process("/dev/video2")
    
 
