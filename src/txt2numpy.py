4de83bdb9a431740dc794f58a123b9bc1d2a1314
Lenovo-HK <56672035@qq.com>
2020-07-20 20:29:46 +0800

    txt2numpy

---
+++ src/txt2numpy.py
@@ -0,0 +1,24 @@
+'''
+@Author: Magician
+@Date: 2020-07-20 16:13:45
+@LastEditors: HK
+@LastEditTime: 2020-07-20 16:16:15
+@Description: file content
+@FilePath: \smartcar2020\src\txt2numpy.py
+'''
+path = "C:/Users/Lenovo/Desktop/fsdownload/data" 
+
+import numpy as np
+
+def txt_2_numpy():
+    angledata = []
+    data = []
+    file = open(path+"/data.txt","r")
+    for line in file.readlines():
+        line = line.strip('\n')
+        angledata.append(int(line))
+    angle = np.array(angledata)
+    np.save(path+"/data.npy", angle,False)
+    file.close()
+
+txt_2_numpy()
